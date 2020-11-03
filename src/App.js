6c401a9bf23f29008ac93d948df43baa1a3ae6b9
saquibb <saquibmsurs@gmail.com>
2020-11-03 12:48:12 +0500

    Header css implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
 import { Products } from './components/Products';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
 import { ProductDetails } from './components/ProductDetails';
 import { NotFound } from './components/NotFound';
 import { ProductIndex } from './components/ProductIndex';
@@ -18,6 +20,8 @@ function App() {
           <Route path='/' element={<ProductIndex />}></Route>
           <Route path=':productID' element={<ProductDetails />}></Route>
         </Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='contact' element={<Contact />}></Route>
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
